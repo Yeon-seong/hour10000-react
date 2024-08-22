8b0447c6917e55e98c20198a0aba8b2483368ee3
yeon-seong <dd300@naver.com>
2024-08-23 02:45:21 +0900

    feat: Header, Main, Footer, Modal 컴포넌트를 화면에 출력 및 css 파일과 연결

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import './App.css';
+import Header from "./components/Header/Header";
+import Main from './components/Main/Main';
+import Footer from './components/Footer/Footer';
+import Modal from './components/Modal/Modal';
+
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+    <>
+      <Header />
+      <Main />
+      <Footer />
+      <Modal />
+    </>
   );
 }
 export default App;
