c0be17a0faf9b84f3c8c63aaba1600eab2bc3d11
yeon-seong <dd300@naver.com>
2024-08-27 23:58:11 +0900

    feat: Modal 컴포넌트 jsx, css 제작

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import lacat from '../../assets/modal_licat.png';
 import './Modal.css';
+
+
 
 export default function Modal() {
   return (
-    <div>Modal</div>
+    <dialog id="modal_container">
+      <form className="modal_form" method="dialog">
+        <h2 className="modal_form_title">화이팅!!♥♥♥</h2>
+        <h3 className="modal_form_subtitle">당신의 꿈을 응원합니다!</h3>
+        <img src={lacat} className="modal_image" alt="초롱초롱한 눈으로 당신의 꿈을 응원하는 라이캣" />
+        <button className="modal_close_button">
+          종료하고 진짜 훈련하러 가기 GO!GO!
+        </button>
+      </form>
+    </dialog>
   )
 }
