34f75fb16233a4fdee4e15dc95e314b762205405
yeon-seong <dd300@naver.com>
2024-08-27 23:56:45 +0900

    feat: Footer 컴포넌트 jsx, css 제작

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import footerLogo from '../../assets/logo_weniv.png';
 import './Footer.css';
+
+
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer id="footer_container">
+      <img src={footerLogo} className="footer_logo" alt="weniv 로고" />
+      <p>&#8251; 본 서비스 내 이미지 및 콘텐츠의 저작권은 주식회사 WeNiv에 있습니다.</p>
+      <p>수정 및 재배포, 무단 도용 시 법적인 문제가 발생할 수 있습니다.</p>
+    </footer>
   )
 }
